5fc402fd3cf1c21071b939381cae6f20375f6e2c
pawelwelfle <pawel.welfle@gmail.com>
2020-05-19 21:33:56 +0200

    Add Dubler

---
+++ src/main/java/edu/iis/mto/search/SearcherDubler.java
@@ -0,0 +1,8 @@
+package edu.iis.mto.search;
+
+public class SearcherDubler implements SequenceSearcher {
+    @Override
+    public SearchResult search(int elem, int[] seq) {
+        return null;
+    }
+}
