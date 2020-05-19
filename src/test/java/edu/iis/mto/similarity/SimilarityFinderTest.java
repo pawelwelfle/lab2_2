eee2345ab7f924fffa0e866dad24c902719c509a
pawelwelfle <pawel.welfle@gmail.com>
2020-05-19 21:32:48 +0200

    Design Tests

---
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -0,0 +1,57 @@
+package edu.iis.mto.similarity;
+
+import org.junit.jupiter.api.Test;
+
+
+class SimilarityFinderTest {
+
+    //testy stanu
+    @Test
+    void CheckIfSequencesAreEmpty() {
+    }
+
+    @Test
+    void CheckIfOneSequenceIsEmpty() {
+    }
+
+    @Test
+    void CheckIfSequenceIsEmpty() {
+    }
+
+    @Test
+    void CheckIfOneElementIsMatching() {
+    }
+
+    @Test
+    void CheckIfNeitherElementIsMatching() {
+    }
+
+    @Test
+    void CheckIfAllElementsMatching() {
+    }
+
+    //testy zachowania
+    @Test
+    void CheckIfSearchMethodInInterfaceIsProperlyUsed() {
+    }
+
+    @Test
+    void CheckIfFirstParamInSearchInterfaceMethodIsProperly() {
+    }
+
+    @Test
+    void CheckIfSecondParamInSearchInterfaceMethodIsProperly() {
+    }
+
+    @Test
+    void CheckIfAllParamsInSearchInterfaceMethodAreUsedProperly() {
+    }
+
+    @Test
+    void CheckIfAllParamsInSearchInterfaceMethodAreNULL() {
+    }
+
+    @Test
+    void CheckIfSearchInterfaceMethodIsEmpty() {
+    }
+}
