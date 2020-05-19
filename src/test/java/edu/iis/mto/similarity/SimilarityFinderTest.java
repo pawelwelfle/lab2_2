e1aad5cac6b280547af8962c1fcdadb092d7c1ca
pawelwelfle <pawel.welfle@gmail.com>
2020-05-20 01:42:59 +0200

    Tests prepare

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -1,33 +1,50 @@
 package edu.iis.mto.similarity;
 
+import edu.iis.mto.search.SearcherDubler;
 import org.junit.jupiter.api.Test;
 
 
 class SimilarityFinderTest {
 
+    SearcherDubler searcherDubler;
+
     //testy stanu
     @Test
     void CheckIfSequencesAreEmpty() {
+        int[] sequenceOne = null;
+        int[] sequenceTwo = null;
+
+
     }
 
     @Test
-    void CheckIfOneSequenceIsEmpty() {
+    void CheckIfFirstSequenceIsEmpty() {
+        int[] sequenceOne = null;
+        int[] sequenceTwo = {1,4,6};
     }
 
     @Test
-    void CheckIfSequenceIsEmpty() {
+    void CheckIfSecondSequenceIsEmpty() {
+        int[] sequenceOne = {1,4,6};
+        int[] sequenceTwo = null;
     }
 
     @Test
     void CheckIfOneElementIsMatching() {
+        int[] sequenceOne = {1,4,6,9};
+        int[] sequenceTwo = {3,2,5,9};
     }
 
     @Test
     void CheckIfNeitherElementIsMatching() {
+        int[] sequenceOne = {1,4,6,8};
+        int[] sequenceTwo = {2,3,5,7};
     }
 
     @Test
     void CheckIfAllElementsMatching() {
+        int[] sequenceOne = {2,7,5,8};
+        int[] sequenceTwo = {2,7,8,5};
     }
 
     //testy zachowania
