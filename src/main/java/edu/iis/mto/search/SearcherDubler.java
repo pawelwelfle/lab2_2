19b5c08af0bc5acb16d2465361516fd1c4a4841f
pawelwelfle <pawel.welfle@gmail.com>
2020-05-20 01:29:49 +0200

    Update Dubler

--- src/main/java/edu/iis/mto/search/SearcherDubler.java
+++ src/main/java/edu/iis/mto/search/SearcherDubler.java
@@ -1,8 +1,37 @@
 package edu.iis.mto.search;
 
+import java.util.HashMap;
+
 public class SearcherDubler implements SequenceSearcher {
+    private HashMap<Integer, Integer> sequence;
+    private int counterOfSearchUse = 0;
+
+    public void setSequence(int[] sequence) {
+        HashMap<Integer, Integer> secondarySeq = new HashMap<>();
+        if (sequence != null) {
+            for (int i = 0; i < sequence.length; i++) {
+                secondarySeq.put(sequence[i], i);
+            }
+        }
+        this.sequence = secondarySeq;
+    }
+
     @Override
     public SearchResult search(int elem, int[] seq) {
-        return null;
+        if (seq == null) {
+            throw new NullPointerException();
+        }
+        counterOfSearchUse++;
+
+        if (this.sequence.containsKey(elem)) {
+            return SearchResult.builder()
+                    .withFound(true)
+                    .withPosition(this.sequence.get(elem))
+                    .build();
+        }
+
+        return SearchResult.builder()
+                .withFound(false)
+                .build();
     }
 }
