6fc5e80b4d1effc63163b67a7d44213cd80b5a56
pawelwelfle <pawel.welfle@gmail.com>
2020-06-06 20:12:21 +0200

    Tests of State

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -3,17 +3,29 @@ package edu.iis.mto.similarity;
 import edu.iis.mto.search.SearcherDubler;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 
 class SimilarityFinderTest {
 
     SearcherDubler searcherDubler;
+    SimilarityFinder similarityFinder;
+    double expResult;
+    double calcResult;
 
     //testy stanu
     @Test
     void CheckIfSequencesAreEmpty() {
         int[] sequenceOne = null;
         int[] sequenceTwo = null;
+        searcherDubler = new SearcherDubler();
+        searcherDubler.setSequence(sequenceTwo);
+        similarityFinder = new SimilarityFinder(searcherDubler);
 
+        assertThrows(
+                NullPointerException.class,
+                () -> this.similarityFinder.calculateJackardSimilarity(sequenceOne, sequenceTwo));
 
     }
 
@@ -21,30 +33,69 @@ class SimilarityFinderTest {
     void CheckIfFirstSequenceIsEmpty() {
         int[] sequenceOne = null;
         int[] sequenceTwo = {1,4,6};
+        searcherDubler = new SearcherDubler();
+        searcherDubler.setSequence(sequenceTwo);
+        similarityFinder = new SimilarityFinder(searcherDubler);
+
+        assertThrows(
+                NullPointerException.class,
+                () -> this.similarityFinder.calculateJackardSimilarity(sequenceOne, sequenceTwo));
     }
 
     @Test
     void CheckIfSecondSequenceIsEmpty() {
         int[] sequenceOne = {1,4,6};
         int[] sequenceTwo = null;
+        searcherDubler = new SearcherDubler();
+        searcherDubler.setSequence(sequenceTwo);
+        similarityFinder = new SimilarityFinder(searcherDubler);
+
+        assertThrows(
+                NullPointerException.class,
+                () -> this.similarityFinder.calculateJackardSimilarity(sequenceOne, sequenceTwo));
     }
 
     @Test
     void CheckIfOneElementIsMatching() {
-        int[] sequenceOne = {1,4,6,9};
-        int[] sequenceTwo = {3,2,5,9};
+        int[] sequenceOne = {1,6,9};
+        int[] sequenceTwo = {3,2,9};
+        expResult = 0.2;
+
+        searcherDubler = new SearcherDubler();
+        searcherDubler.setSequence(sequenceTwo);
+        similarityFinder = new SimilarityFinder(searcherDubler);
+        calcResult = similarityFinder.calculateJackardSimilarity(sequenceOne,sequenceTwo);
+
+        assertEquals(calcResult, expResult);
     }
 
     @Test
     void CheckIfNeitherElementIsMatching() {
         int[] sequenceOne = {1,4,6,8};
         int[] sequenceTwo = {2,3,5,7};
+        expResult = 0.0;
+
+        searcherDubler = new SearcherDubler();
+        searcherDubler.setSequence(sequenceTwo);
+        similarityFinder = new SimilarityFinder(searcherDubler);
+        calcResult = similarityFinder.calculateJackardSimilarity(sequenceOne,sequenceTwo);
+
+        assertEquals(calcResult, expResult);
     }
 
     @Test
     void CheckIfAllElementsMatching() {
         int[] sequenceOne = {2,7,5,8};
         int[] sequenceTwo = {2,7,8,5};
+        expResult = 1.0;
+
+        searcherDubler = new SearcherDubler();
+        searcherDubler.setSequence(sequenceTwo);
+        similarityFinder = new SimilarityFinder(searcherDubler);
+        calcResult = similarityFinder.calculateJackardSimilarity(sequenceOne,sequenceTwo);
+
+        assertEquals(calcResult, expResult);
+
     }
 
     //testy zachowania
